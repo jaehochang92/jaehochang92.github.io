6ce1d8f9862002afcaa7fa68955462b0065e85bc
jaehochang <cjhsfl@gmail.com>
2021-02-22 19:20:15 +0900

    playing around with gradcafe monitoring

--- _pages/gcm/main.py
+++ _pages/gcm/main.py
@@ -3,17 +3,20 @@ from urllib.request import urlopen
 import re
 import pandas
 
-
-with urlopen('https://www.thegradcafe.com/survey/index.php?'
-             'q=duke+biostat*&t=a&o=&pp=50') as response:
+with open('/Users/jangjaeho/jae-github/jaehochang92.github.io/_pages/gcm/query.txt', 'r') as q:
+    query = q.readlines()[0].replace('\n', '').replace(' ', '+')
+with urlopen(f'https://www.thegradcafe.com/survey/index.php?q={query}&t=a&o=&pp=250') as response:
     html = response.read().decode()
 submissions = re.compile('<table class="submission-table">(.*?)</table>', re.DOTALL).findall(html)[0]
 submissions = re.sub('(\\n|\\t)', '', submissions)
 inst = re.findall('instcol tcol1\">(.*?)</td>', submissions)
 prgm = re.findall('<td class="tcol2">(.*?)</td>', submissions)
 stus = re.findall('<td class="tcol3 (.*?)>(.*?)</td>', submissions)
+stus = [re.findall('(Accepted|Rejected|Other|Interview|Wait listed).+via (.+) on (.+[0-9]{4})', tuple[1])[0] for tuple in stus]
 dgre = re.findall('<td class="tcol4">(.*?)</td>', submissions)
 sdte = re.findall('<td class="datecol tcol5">(.*?)</td>', submissions)
 cmnt = re.findall('<li>(.*?)</li>', submissions)
 if len(inst) == len(prgm) == len(stus) == len(dgre) == len(sdte) == len(cmnt):
-    pprint(pandas.DataFrame([*zip(inst, prgm, stus, dgre, sdte, cmnt)]))
+    rows = [(j, prgm[i], *stus[i], dgre[i], sdte[i], cmnt[i]) for i, j in enumerate(inst)]
+    df = pandas.DataFrame(rows, columns=['학교', '과정', '결과', 'via', 'on', '출신', '게시날짜', '코멘트'])
+    df.to_csv('/Users/jangjaeho/jae-github/jaehochang92.github.io/_pages/gcm/tmp.csv')
