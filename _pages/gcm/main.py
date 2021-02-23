36eb93c45f12e7da4c349ed2d364e6e2da852b56
jaehochang <cjhsfl@gmail.com>
2021-02-23 21:41:10 +0900

    gradcafe monitor update

--- _pages/gcm/main.py
+++ _pages/gcm/main.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     cmnt = re.findall('<li>(.*?)</li>', submissions)
 
     if len(inst) == len(prgm) == len(stus) == len(dgre) == len(sdte) == len(cmnt):
-        col_names = ['학교', '과정', '결과', 'via', 'on', '출신', '게시날짜', '코멘트']
+        col_names = ['inst', 'prgrm', 'res', 'via', 'on', 's', 'date', 'cmmnts']
         rows = [(j, prgm[i], *stus[i], dgre[i], sdte[i], cmnt[i][:100])
                 for i, j in enumerate(inst)]
         df = pandas.DataFrame(rows, columns=col_names)
