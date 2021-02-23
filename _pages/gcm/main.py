b4c3514f9d0f184aadc89ffeb0196a829526f32a
jaehochang <cjhsfl@gmail.com>
2021-02-23 13:27:19 +0900

    gcm updated

--- _pages/gcm/main.py
+++ _pages/gcm/main.py
@@ -1,9 +1,27 @@
 from pprint import pprint
 from urllib.request import urlopen
 import re
-import pandas
+import argparse
 
-query = input('Type your query: ').replace(' ', '+')
+parser = argparse.ArgumentParser(description='Type your query.')
+parser.add_argument('-i', '--institution')
+parser.add_argument('-p', '--program')
+parser.add_argument('-d', '--degree', choices=['phd', 'mas'])
+args = parser.parse_args()
+
+query = ''
+if args.institution:
+    query += '+' if query else ''
+    query += args.institution
+if args.program:
+    query += '+' if query else ''
+    query += args.program
+print(query)
+degree_dict = {'phd':'PhD', 'mas':'Master'}
+if args.degree:
+    dgr = degree_dict[args.degree]
+print(dgr)
+
 with urlopen(f'https://www.thegradcafe.com/survey/index.php?q={query}&t=a&o=&pp=250') as response:
     html = response.read().decode()
 submissions = re.compile('<table class="submission-table">(.*?)</table>', re.DOTALL).findall(html)[0]
@@ -16,8 +34,7 @@ dgre = re.findall('<td class="tcol4">(.*?)</td>', submissions)
 sdte = re.findall('<td class="datecol tcol5">(.*?)</td>', submissions)
 cmnt = re.findall('<li>(.*?)</li>', submissions)
 if len(inst) == len(prgm) == len(stus) == len(dgre) == len(sdte) == len(cmnt):
+    col_names=['학교', '과정', '결과', 'via', 'on', '출신', '게시날짜', '코멘트']
     rows = [(j, prgm[i], *stus[i], dgre[i], sdte[i], cmnt[i]) for i, j in enumerate(inst)]
-    df = pandas.DataFrame(rows, columns=['학교', '과정', '결과', 'via', 'on', '출신', '게시날짜', '코멘트'])
-    df.to_csv('/Users/jangjaeho/jae-github/jaehochang92.github.io/_pages/gcm/tmp.csv')
-
-print(df.head(10))
+    print(col_names)
+    print(rows)
